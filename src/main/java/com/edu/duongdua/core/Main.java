720bacd98706f53d110bd57eb1092fa70489ae06
GaoSoiBac <tomnguyenhieu2004@gmail.com>
2025-01-07 02:14:59 +0700

    Update Main.java

--- src/main/java/com/edu/duongdua/core/Main.java
+++ src/main/java/com/edu/duongdua/core/Main.java
@@ -1,24 +1,9 @@
 package com.edu.duongdua.core;
 
 import com.edu.duongdua.core.controller.LoginController;
-import com.edu.duongdua.core.view.Scene_SideBar;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TitledPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
